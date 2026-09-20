fa975d11c882c0444db47cc60dd2614b50022ebe
Daffa Shiddiq <mendezdez12@gmail.com>
2026-09-20 21:45:25 +0700

    Clean up and standardize .http test file structure

---
+++ DaffaCatering.API/DTOs/PelangganDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DaffaCatering.API.DTOs
+{
+    public class PelangganDto
+    {
+        [Required(ErrorMessage = "ID Pelanggan wajib diisi")]
+        [StringLength(10, ErrorMessage = "ID Pelanggan maksimal 10 karakter")]
+        public string IdPelanggan { get; set; } = null!;
+
+        [Required(ErrorMessage = "Nama Pelanggan wajib diisi")]
+        [StringLength(50, ErrorMessage = "Nama Pelanggan maksimal 50 karakter")]
+        public string NamaPelanggan { get; set; } = null!;
+
+        [StringLength(20)]
+        public string? NoKontak { get; set; }
+
+        [StringLength(255, ErrorMessage = "Alamat maksimal 255 karakter")]
+        public string? Alamat { get; set; }
+
+        public DateOnly TglBergabung { get; set; }
+
+        public bool Status { get; set; }
+    }
+}
